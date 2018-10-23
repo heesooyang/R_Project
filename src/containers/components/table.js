3fe83e02379afb977c9f55733e62f2fe978c97f2
YHS <heesoo@slogup.com>
2018-10-23 15:01:53 +0900

    for test

---
+++ src/containers/components/table.js
@@ -0,0 +1,19 @@
+import { HashRouter as Router, Route, Switch, Link, withRouter } from 'react-router-dom';
+import { Breadcrumb, Alert } from 'antd';
+import React, { Component } from 'react'
+
+class Table extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+MyComponent.propTypes = {}
+
+export default MyComponent
+
+
